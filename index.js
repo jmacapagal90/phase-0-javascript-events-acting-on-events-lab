9cab1d9be40f025c611b13ddaa1311d67d710a83
James Macapagal <j.macapagal@gmail.com>
2022-05-22 20:55:41 -0500

    commit tree a4c32f170dd169681d667179956d62762582a5a7

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.bottom = "50px"
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  left > 0 ? dodger.style.left = `${left - 1}px`:dodger.style.left = "0px";
+}
+
+document.addEventListener("keydown", e => e.key === "ArrowLeft" ? moveDodgerLeft() : false);
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  left < 360 ? dodger.style.left = `${left + 1}px`:dodger.style.left = "0px";
+}
+
+document.addEventListener("keydown", e => e.key === "ArrowRight" ? moveDodgerRight() : false);
